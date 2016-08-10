1934b2d5ed31d088f8d462e04b2cc904ca2841da
Gene Callahan <gcallah@mac.com>
2016-08-10 22:27:48 +0000

    Real file instead of link.

--- setup_server.sh
+++ setup_server.sh
@@ -1,1 +1,10 @@
-../setup_server.sh
+#/bin/sh
+
+echo "Getting berkeley code from GitHub."
+git clone https://github.com/gcallah/berkeley.git
+mv berkeley mysite
+
+
+echo "The following must be run interactively: once you run them, run mysite/db.sh."
+echo "mkvirtualenv --python=/usr/bin/python3.5 django19"
+echo "workon django19"
