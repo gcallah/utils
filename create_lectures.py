2c1309e70aacc0a6461bca928bbdc68b56db1c4b
gcallah <gcallah@mac.com>
2016-12-22 00:49:56 -0500

    Creating scripts to generate lecture pages.

--- create_lectures.py
+++ create_lectures.py
@@ -6,7 +6,8 @@ from subprocess import call
 
 HTML_PG = 0
 TITLE = 1
-LECTURE_TEMPLATE = "LectureTempl.py"
+LECTURE_TEMPLATE = "../utils/LectureTempl.html"
+LECTURE_SCRIPT = "../utils/create_lecture.py"
 
 
 if len(sys.argv) < 2:
@@ -16,6 +17,8 @@ if len(sys.argv) < 2:
 with open(sys.argv[1], "r") as f_in:
     freader = csv.reader(f_in)
     for row in freader:
-        print("Html page = " + row[HTML_PG] + "; title = " + row[TITLE])
-        call("./create_lecture.py <" + LECTURE_TEMPLATE +
-            " >" + row[HTML_PG] + " " + row[TITLE])
+        print("Creating html page = " + row[HTML_PG] + "; title = " + row[TITLE])
+        call(LECTURE_SCRIPT + " " + row[TITLE] +
+             " <" + LECTURE_TEMPLATE +
+             " >" + row[HTML_PG],
+             shell=True)
