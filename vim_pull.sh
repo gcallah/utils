dc1ec6405b9c5a1a5a91a933245655893e222519
gcallah <gcallah@mac.com>
2017-04-29 18:05:35 -0400

    removed vim shell scripts.

--- vim_pull.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-git pull origin master
-
-cp .vimrc ~
-cp *.vim ~/.vim/after/ftplugin
--- vim_push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cp ~/.vimrc .
-cp ~/.vim/after/ftplugin/*.vim .
-
-git commit .vimrc *.vim -m "Updating vim scripts."
-git push origin master
