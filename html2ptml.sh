c60c8287f93369f11548a5d88a7955d26f716527
janeilpatel94 <janeilpatel94@gmail.com>
2018-02-13 16:14:30 -0500

    Working version

--- html2ptml.sh
+++ html2ptml.sh
@@ -3,5 +3,12 @@ for f in $FILES
 do
   echo "Processing $f file..."
   # you are going to run:
+  	filename=$(basename "$f")
+	#echo "Processing $filename file..."
+	extension="${filename##*.}"
+	#echo "Processing $extension file..."
+	filename="${filename%.*}"
+	#echo "Processing $filename file..."
+  	../utils/html2ptml.awk < $f > test/$filename.ptml
   # html2ptml.awk <$f > (What goes here?!)
 done
