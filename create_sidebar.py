58d6101e53bb0ac469ad0c0908a8f9d3b1750497
qiweizhou94 <qiweizhou94@gmail.com>
2018-04-09 13:07:33 -0400

    update code to support title creation

--- create_sidebar.py
+++ create_sidebar.py
@@ -1,6 +1,9 @@
 import sys
 
 def parseLine(line):
+    if not line.strip():
+        return [-1, "", None, None]
+
     # count stars
     star_count = 0
     for char in line:
@@ -8,22 +11,30 @@ def parseLine(line):
             star_count += 1
         else:
             break
-
-    # save section name
+    # save title and section name
     splitted_line = line.split('^')
-    section_name = splitted_line[0][star_count:]
-
-    # save url and glyphicon if there is one
-    # returns [star_count, section_name, url, glyphicon]
-    if len(splitted_line) > 2:
-        return [star_count, section_name, splitted_line[1], splitted_line[2]]
-    elif len(splitted_line) > 1:
-        if splitted_line[1].startswith("glyphicon"):
-            return [star_count, section_name, None, splitted_line[1]]
-        else:
-            return [star_count, section_name, splitted_line[1], None]
+    if star_count == 0:
+        # for title
+        if len(splitted_line) != 2:
+            print "ERROR: Bad input at %s. You must specify full-title and short-title." % line
+            sys.exit()
+        return [star_count, splitted_line[0], splitted_line[1]]
     else:
-        return [star_count, section_name, None, None]
+        section_name = splitted_line[0][star_count:]
+        # save url and glyphicon if there is one
+        # returns [star_count, section_name, url, glyphicon]
+        if len(splitted_line) > 3:
+            print "ERROR: Bad input at %s. At most 3 ^ separators are accepted." % line
+            sys.exit()
+        elif len(splitted_line) > 2:
+            return [star_count, section_name, splitted_line[1], splitted_line[2]]
+        elif len(splitted_line) > 1:
+            if splitted_line[1].startswith("glyphicon"):
+                return [star_count, section_name, None, splitted_line[1]]
+            else:
+                return [star_count, section_name, splitted_line[1], None]
+        else:
+            return [star_count, section_name, None, None]
 
 
 def create_line_with_spaces(n, str):
@@ -34,6 +45,9 @@ def create_nested_list(lines):
         print "UNEXPECTED ERROR: Empty list in recursion"
         sys.exit()
     start_star_count = lines[0][0]
+    if start_star_count == 0:
+        print "ERROR: Bad input at %s. Star count should be at least 1 in contents." % lines[0]
+        sys.exit()
     start_index = 0
     end_index = 0
     level = []
@@ -42,8 +56,8 @@ def create_nested_list(lines):
             end_index += 1
         if lines[end_index][2] is None:
             end_index += 1
-            if lines[end_index][0] <= start_star_count:
-                print "ERROR: Bad input format at %s" % lines[end_index][1]
+            if lines[end_index][0] != start_star_count + 1:
+                print "ERROR: Bad input at %s. Star count does not match context." % lines[end_index][1]
                 sys.exit()
             while end_index < len(lines) and lines[end_index][0] > start_star_count:
                 end_index += 1
@@ -54,22 +68,34 @@ def create_nested_list(lines):
         start_index = end_index
     return level
 
-input_fname = "sidebar_input_sample.txt"
-output_fname = "sidebar_output_sample.txt"
+# TO DO: should read file names from command line input
+input_fname = "test_data/menu_OOP.txt"
+output_fname = "create_sidebar_output.txt"
 with open(input_fname) as f:
     lines = f.readlines()
 lines = [line.rstrip('\n') for line in lines]
 lines = [line.rstrip('\r') for line in lines] # for windows machines
 
-parsed_lines = [parseLine(line) for line in lines]
+parsed_lines = []
+for line in lines:
+    parsed_line = parseLine(line)
+    # discard empty lines
+    if parsed_line[0] > -1:
+        parsed_lines.append(parsed_line)
 
-# create a nested list
-nested = create_nested_list(parsed_lines[2:len(parsed_lines)-1])
+title_line = parsed_lines[0]
+# title is required
+if title_line[0] != 0:
+    print "ERROR: Bad input. No title found. Add a new line of <full-title>^<short-title> to the head of your input file."
+    sys.exit()
+
+# create a nested list with contents
+nested = create_nested_list(parsed_lines[1:])
 
 # write generated sidebar
 with open(output_fname, 'w+') as f:
     # write title
-    f.write("<!-- Sidebar Holder -->\n<nav id=\"sidebar\">\n    <div id=\"sidebarCollapse\">\n        <div class=\"sidebar-header\">\n            <h1>%s</h1>\n            <strong>%s</strong>\n        </div>\n    </div>\n" % (lines[0][1], lines[1][1]))
+    f.write("<!-- Sidebar Holder -->\n<nav id=\"sidebar\">\n    <div id=\"sidebarCollapse\">\n        <div class=\"sidebar-header\">\n            <h1>%s</h1>\n            <strong>%s</strong>\n        </div>\n    </div>\n" % (title_line[1], title_line[2]))
 
     # write lists
     f.write("    <ul class=\"list-unstyled components\">\n")
