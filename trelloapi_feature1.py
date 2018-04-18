9cbd81ab7279c112e05ac09bc751a5800129beb9
Jayesh Mhatre <jkm437@nyu.edu>
2018-04-18 19:34:15 -0400

    Updated the code with mypy type annotations!

--- trelloapi_feature1.py
+++ trelloapi_feature1.py
@@ -12,14 +12,19 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+try:
+    from typing import List,Set
+except ImportError:
+    print("WARNING: Typing module is not found! Kindly install the latest version of python!")
+
 # extracting the present time
 time_now = datetime.datetime.utcnow()
 
 # setting a days of inactivity limit
-days_of_inactivity_limit = 14 #default value: 14 days
+days_of_inactivity_limit = 14 # type: int #default value: 14 days
 
 # trying to read all the boards where I am a member
-url_member = "https://api.trello.com/1/members/dsd2981"
+url_member = "https://api.trello.com/1/members/dsd2981" # type :str
 querystring = {"key":"b282952c1211b7eb3c16b7c3adfbbf7f","token":"12f1ebbfd62746257dbfb66c07ce42d1240d0a0cf0d1959b5706f411edd6315d"}
 response_member = requests.request("GET", url_member, params=querystring)
 
@@ -27,23 +32,23 @@ response_member = requests.request("GET", url_member, params=querystring)
 data_member = json.loads(response_member.text)
 
 # removing the first 2 boards as they are my personal boards
-board_ids = data_member['idBoards'][2:]
+board_ids = data_member['idBoards'][2:] # type: List[str]
 
 # storing email body in message string
-message = "The following cards have not been moved from past 14 days \n \n"
+message = "The following cards have not been moved from past 14 days \n \n" # type: str
 
 # reading cards of a single board using the board id
 
 for i in range(0, len(board_ids)):
 
     #retrieving the name of the board using board id
-    url_board_name = "https://api.trello.com/1/boards/" + board_ids[i] +"/name"
+    url_board_name = "https://api.trello.com/1/boards/" + board_ids[i] +"/name" # type :str
     response_board_name = requests.request("GET", url_board_name, params=querystring)
     board_name = json.loads(response_board_name.text)
-    message += "Cards in "  + board_name['_value'] + ": \n"
+    message += "Cards in "  + board_name['_value'] + ": \n" # type :str
 
     # retreiving cards from board using board id
-    url_board_cards = "https://api.trello.com/1/boards/" + board_ids[i] +"/cards"
+    url_board_cards = "https://api.trello.com/1/boards/" + board_ids[i] +"/cards" # type :str
     response_board_cards = requests.request("GET", url_board_cards, params=querystring)
     data_board_cards = json.loads(response_board_cards.text)
 
@@ -57,18 +62,18 @@ for i in range(0, len(board_ids)):
         time_difference = time_now - card_latest_activity_timestamp
 #         print(time_difference.seconds)
         # extracting name of list of card using the list id
-        url_card_list = "https://api.trello.com/1/lists/" + data_board_cards[i]['idList'] + "/name"
+        url_card_list = "https://api.trello.com/1/lists/" + data_board_cards[i]['idList'] + "/name" # type :str
         response_card_list = requests.request("GET", url_card_list, params=querystring)
         data_card_list = json.loads(response_card_list.text)
-        card_list_name = data_card_list['_value']
+        card_list_name = data_card_list['_value']  # type: List[str]
 
         # finding whether card previous activity was more than or equal to days_of_inactivity_limit
         # and card is not present in wish list and done list
         if(time_difference.days >= days_of_inactivity_limit and card_list_name not in ('Wish List', 'Done')):
-            message += data_board_cards[i]['name'] + " " + data_board_cards[i]['shortUrl'] + "\n"
-    message += "\n"
+            message += data_board_cards[i]['name'] + " " + data_board_cards[i]['shortUrl'] + "\n" # type :str
+    message += "\n" # type :str
 
-message += "Note: Removed cards in Wish List and Done List"
+message += "Note: Removed cards in Wish List and Done List" # type :str
 
 # set up the SMTP server
 s = smtplib.SMTP(host='smtp.gmail.com', port=587)
@@ -80,13 +85,13 @@ text_file = open("EMAIL_INFO.txt","r")
 lines = text_file.read().split(' ')
 s.login(user=lines[0], password=lines[1])
 
-to_contacts = ["dsd298@nyu.edu", "ejc369@nyu.edu"]
+to_contacts = ["dsd298@nyu.edu", "ejc369@nyu.edu"] # type :List[str]
 for i in range(0, len(to_contacts)):
     msg = MIMEMultipart()       # create a message
     # message = "this is a test"
-    msg['From'] = "devopsnyu@gmail.com"
-    msg['To'] = to_contacts[i]
-    msg['Subject'] = "Card Inactivity Information E-Mail"
+    msg['From'] = "devopsnyu@gmail.com" # type :List[str]
+    msg['To'] = to_contacts[i] # type :List[str]
+    msg['Subject'] = "Card Inactivity Information E-Mail" # type :List[str]
 
     # add in the message body
     msg.attach(MIMEText(message, 'plain'))
