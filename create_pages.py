d92d529c6130be7a3d5d816b0de4a8edcb129e47
gcallah <gcallah@mac.com>
2018-04-19 00:53:55 -0400

    Working on page creation script.

--- create_pages.py
+++ create_pages.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python3 
 
 import sys
-from pylib.parse_course import parse_course
 from subprocess import call
+from pylib.parse_course import parse_course, CourseItem
 try:
     from typing import List
 except ImportError:
     print("WARNING: Typing module is not find")
-HTML_PG = 0 # type: int
-TITLE = 1 # type: int
-OPEN_ERROR = 1 # type: int
+
+HTML_PG = 0  # type: int
+TITLE = 1  # type: int
+OPEN_ERROR = 1  # type: int
 PAGE_SCRIPT = "../utils/create_page.py"   # type: str
 
+HTML_EXT = "html"  # type: str
+PTML_EXT = "ptml"  # type: str
+PTML_DIR = "html_src"  # type: str
 
 if len(sys.argv) < 3:
     print("Must supply a file of pages to create and a page template.")
     exit(1)
 
-pages = sys.argv[1]
+pages = []
+pages_file = sys.argv[1]
 page_templ = sys.argv[2]  # type: str
 
 try:
-    with open(pages, "r") as f_in:
-        freader = csv.reader(f_in) 
-        for row in freader:
-            print("<a href=\"" + row[HTML_PG] + "\">\n    "
-                  + row[TITLE] + "\n</a>\n")
-            call(PAGE_SCRIPT + " \"" + row[TITLE] +
-                 "\" <" + page_templ +
-                 " >" + row[HTML_PG],
-                 shell=True)
+    pages = parse_course(pages_file)
 except:
-    print("ERROR: Failed to open " + pages)
+    print("ERROR: Failed to open " + pages_file)
     exit(OPEN_ERROR)
+
+for course_module in pages:
+    if course_module.url is not None:
+        pass
+
