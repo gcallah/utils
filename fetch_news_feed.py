008d92d8229a1db667c2a896114458449a3cb619
Chaoqun Huang <jerryhuangchaoqun@gmail.com>
2018-04-24 17:09:23 -0400

    add write back to index.html

--- fetch_news_feed.py
+++ fetch_news_feed.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import urllib2
 from bs4 import BeautifulSoup
 
@@ -12,7 +14,21 @@ request = urllib2.Request("https://devops.com", None, headers) # type: object
 response = urllib2.urlopen(request, None, 5) # type: object
 html = response.read() # type: object
 soup = BeautifulSoup(html) # type: object
+news = {}
+
 for tag in soup.find_all(attrs={"class": "slides"}):
     for atag in tag.find_all('a'):
-        print atag['href']
-        print atag.find('img')['src']
+        print (atag['href'])
+        print (atag.find('img')['src'])
+        news[atag['href']] = atag.find('img')['src']
+
+index_page = BeautifulSoup(open("../index.html"), "html.parser")
+count = 0
+
+for tag in index_page.find_all(attrs={"class": "carItem"}):
+    tag['onclick'] = "window.location.href='" + list(news.keys())[count] + "'"
+    tag['src'] = news[list(news.keys())[count]]
+    count += 1
+
+with open("../index.html", "w") as file:
+    file.write(index_page.prettify().encode('utf-8').strip())
