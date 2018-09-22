880315f1cc96bc0481f1cc3fa380de3b200b158c
cyong813 <cy986@nyu.edu>
2018-09-21 20:49:59 -0400

    Second commit, produce file with htmlof definitions

--- glossbuilder.py
+++ glossbuilder.py
@@ -15,6 +15,10 @@ exit_error = False # type: bool
 strict_mode = False # type: bool
 file_nm = None
 
+INDENT1 = "        " # type: str
+INDENT2 = INDENT1 + INDENT1 # type: str
+INDENT3 = INDENT2 + INDENT1 # type: str
+
 def check_file(*files): #check if file exists
     for file in files:
         if not os.path.isfile(file):
@@ -37,14 +41,21 @@ saw_error = False  # type: bool
 d = dict()  # type: Dict[str]
 code_tag_on = False  # type:bool
 
-with open('definitions.txt', 'r') as f:
-    for line in f:
-       term = line.strip().split("  ")
-       d[term[0]] = term[1]
+with open('/test_data/glossbuilder-definitions-test.txt', 'r') as f:
+    try:
+        #place terms/defs in dictionary
+        for line in f:
+            term = line.strip().split("  ")
+            d[term[0]] = term[1]
+    except IndexError:
+        print("Are you sure every term has its own definition?")
 
-#create glossary file and clear set d
+#create glossary file by using dictionary
 with open('glossary.html', 'a+') as f:
-    f.writelines(i + '\n' for i in d)
+    f.write('<ul class="nested">') #open ul
+    for key in d:
+        f.writelines(INDENT1+'<li>'+'\n'+INDENT2+'<a name='+key+'>'+'<span class="hilight">'+key+'</span>:'+'\n'+INDENT2+'</a>'+'/n'+INDENT2+d[key]+'\n'+'</li>')
+    f.write('</ul>') #close ul
     d.clear()
 
 if exit_error:
