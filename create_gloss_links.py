41725917f8799b4cbd35e9a3fc3f4137587ebd5f
Monjur Hasan <monjur.nishad@gmail.com>
2018-10-06 00:07:55 -0400

    Indexer part 1
    edge cases not handles yet.

--- create_gloss_links.py
+++ create_gloss_links.py
@@ -1,49 +1,73 @@
 import os
 import argparse
-import csv
-import shutil
 ARG_ERROR = 1  # type: int
-file_name = None
+"""
+So far: the indexer takes a keyword, and a list of files. Parses all the files
+and prints all context of the keyword.
 
+current limitation: if keyword finishes with a punctuation, doesn't match. If appears more than
+once in a line, only shows once. 
 
-def check_file(*files): #check if file exists
+Discuss: should i take list of files as list in command list? 
+
+"""
+
+def check_file(files): #check if file exists
     for file in files:
         if not os.path.isfile(file):
             print(file + " is not a file")
             exit(ARG_ERROR)
 
-
 if __name__ == '__main__':
 
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("text_file")
-    arg_parser.add_argument("html_file")
+    arg_parser.add_argument("key")
+    arg_parser.add_argument("list_files")
     args = arg_parser.parse_args()
-    text_file = args.text_file
-    html_file = args.html_file
+    keyword = args.key
+    list_files = args.list_files
 
-check_file(text_file,html_file)
 
-text_path, html_path = (os.path.abspath(text_file)), (os.path.abspath(html_file))
-glossary = []
+list_files = list_files.strip('[]').split(',')
+check_file(list_files)
 
-with open(text_path) as txt:
-    try:
+keyword = keyword.lower()
+index_dict = {}
+
+for file in list_files:
+    with open(file, 'r') as txt:
+
         for line in txt:
-            glossary.append(line)
+            line = line.lower()
 
-    except IndexError:
-        print("something went wrong")
+            if keyword in line:
+                line = line.strip().split(" ")
+                context = None
+                key_index = line.index(keyword)
+
+                if 0 < key_index < len(line) - 1:
+
+                    context = line[key_index-1] + " " + line[key_index] + " " + line[key_index+1]
+
+                elif key_index == 0:
+                    if len(line) > 1:
+                        context = line[key_index] + " " + line[key_index+1]
+                    else:
+                        context = line[key_index]
+
+                elif key_index == len(line) - 1:
+                    context = line[key_index - 1] + " " + line[key_index]
+
+                if file not in index_dict:
+                    index_dict[file] = []
+
+                index_dict[file].append(context)
+
+for key, value in index_dict.items():
+    print(keyword + " occurs in: ")
+    for each_context in value:
+        print("    " + key + ": " + each_context)
 
 
-with open(html_path) as f, open("new_html_path", 'w') as fout:
-    rdr = csv.reader(f)
-    wrt = csv.writer(fout)
 
-    for line in rdr:
-        if line[0] in glossary:
-            line[1] = str(int(line[1]) - 20)
-        wrt.writerow(line)
-
-shutil.copyfile("new_html_path", html_path)
-os.remove("new_html_path")
+#python3 create_gloss_links.py "description" ["data_manip.html","index.html"]
