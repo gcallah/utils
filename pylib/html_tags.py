ef269291bcecb1c217b40addf5670ecff78cca24
gcallah <gcallah@mac.com>
2018-10-11 14:42:21 -0400

    Restored list functions to html_tags.

--- pylib/html_tags.py
+++ pylib/html_tags.py
@@ -40,3 +40,47 @@ def details(sumtext, level=1, indent=INDENT4, inc_par=False, inc_fig=False,
     s += indent + '</details>\n'
     return s
 
+def ulist(css_class=None, l=None, indent=INDENT4, level=1):
+    return html_list(css_class=css_class, l=l,
+                     indent=indent, level=level,
+                     list_type='ol')
+
+def olist(css_class=None, l=None, indent=INDENT4, level=1):
+    return html_list(css_class=css_class, l=l,
+                     indent=indent, level=level,
+                     list_type='ol')
+
+def html_list(css_class=None, l=None, indent=INDENT4, level=1,
+              list_type='ul'):
+    # represents modules in homepage
+    indent += INDENT1 * (level - 1)
+    inner_indent = indent + INDENT1   # add one level indentation
+    s = indent + "<" + list_type
+    if css_class is not None:
+        s += " class=\"" + css_class + "\""
+    s += ">\n"
+    for item in l:
+        # l is the content within li tag (in this case, anchor tags)
+        s += inner_indent
+        s += "<li>\n"
+        s += inner_indent
+        s += item + "\n"
+        s += inner_indent
+        s += "</li>\n"
+    s += indent + "</" + list_type + ">\n"
+    return s
+
+def image(indent=INDENT4, src="", alt="", other_attr=""):
+    s = indent + "<img src=" + src + "alt=" + alt + other_attr + ">\n"
+    return s
+
+def head(indent=INDENT4, title="", cssFile=None): # generate meta head
+    s = "<head>\n"
+    s += INDENT1 + "<title>\n\n"
+    s += INDENT2 + title + "\n"
+    s += INDENT1 + "</title>\n"
+    if cssFile is not None:
+        s += "<link rel='stylesheet' type='text/css' href="+cssFile+">\n"
+    s += "</head>\n"
+    return s
+
