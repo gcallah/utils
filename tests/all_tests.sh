cda6ea67e9504ba7b74f42fb884864a326a76efe
gcallah <gcallah@mac.com>
2018-10-12 17:48:08 -0400

    Took out commented out code from all_tests.sh

--- tests/all_tests.sh
+++ tests/all_tests.sh
@@ -26,8 +26,3 @@ run_diff_test_std create_page "$title"
 run_diff_test_file create_gloss
 run_diff_test_file create_menu
 
-#./create_menu.py $TEST_DATA/create_menu_inp.txt $TEST_DATA/create_menu_tmp.txt
-#diff $TEST_DATA/create_menu_out.txt $TEST_DATA/create_menu_tmp.txt
-#echo "Create menu test passed."
-#rm $TEST_DATA/create_menu_tmp.txt
-
