9fe30cb989e8a79a4e4984720511f407ce2aabc7
gcallah <gcallah@mac.com>
2018-10-12 17:39:01 -0400

    Regularizing tests of utils.

--- tests/all_tests.sh
+++ tests/all_tests.sh
@@ -3,6 +3,19 @@
 # exit on any error with that error status:
 set -e
 
+run_diff_test() {
+    echo "Running ./$1.py $2 < $TEST_DATA/$1_inp.txt > $TEST_DATA/$1_tmp.txt"
+    ./"$1.py" "$2" < $TEST_DATA/$1_inp.txt > $TEST_DATA/$1_tmp.txt
+    echo "Running diff between $TEST_DATA/$1_out.txt and $TEST_DATA/$1_tmp.txt."
+    diff $TEST_DATA/$1_out.txt $TEST_DATA/$1_tmp.txt
+    echo "$1 passed."
+    rm $TEST_DATA/$1_tmp.txt
+}
+
+export title="Test page"
+run_diff_test create_page "$title"
+
+
 ./create_gloss.py $TEST_DATA/create_gloss_inp.txt > $TEST_DATA/create_gloss_tmp.txt
 diff $TEST_DATA/create_gloss_out.txt $TEST_DATA/create_gloss_tmp.txt
 echo "Create gloss test passed."
