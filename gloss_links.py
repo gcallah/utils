03c41e7f824570b2162dea5a60c3af57b4f62e73
gcallah <gcallah@mac.com>
2018-10-15 12:15:16 -0400

    Refactoring gloss links

--- gloss_links.py
+++ gloss_links.py
@@ -24,90 +24,107 @@ test_data/<output directory>.<keyword>.txt
 
 """
 
-if __name__ == '__main__':
+def process_file(filenm, contexts_per_file):
+    """
+    Args: filenm and contexts_per_file
+    returns: None
+    """
+    try:
+        with open(filenm, 'r') as txt:
+            for line in txt:
+    
+                # splits into a list
+                if keyword in line:
+                    line = line.strip().split(" ")
+                    context = None
+                    index_list = []
+    
+                    # iterate over list to handle edge case when 
+                    # keyword ends with punctuation
+                    for index, word in enumerate(line):
+                        if keyword in word:
+                            index_list.append(index)
+    
+                    for index in index_list:
+                        # if keyword appears more than once in a line
+                        key_index = index
+    
+                        if 0 < key_index < len(line) - 1:
+                            context = (line[key_index-1] + " " +
+                                    line[key_index] + " " +
+                                    line[key_index+1])
+    
+                        elif key_index == 0:
+                            if len(line) > 1:
+                                context = (line[key_index] + " " + 
+                                        line[key_index+1])
+                            else:
+                                context = line[key_index]
+    
+                        elif key_index == len(line) - 1:
+                            context = (line[key_index - 1] + " " + 
+                                    line[key_index])
+                        contexts_per_file[file].append(context)
+    except IOError as ioe:
+        print("Error opening file: %s; exception: %s", (filenm, str(ioe)))
+        return None
 
+def process_args():
+    """
+    Parses command line args and returns:
+        keyword_file, file_list
+    """
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("gloss_key")
-    arg_parser.add_argument("output")
-    #arg_parser.add_argument("list_files")
+    arg_parser.add_argument("outdir")
     arg_parser.add_argument(
-        "--lf", #you need to add "--lf" flag in command line
+        "--lf", # you need to add "--lf" flag in command line
         nargs="*",
         type=str,
         default=[],
     )
     args = arg_parser.parse_args()
-    keyword = args.gloss_key
-    list_files = args.lf
-    output_file = args.output
+    keyword_file = args.gloss_key
+    outdir = args.outdir
+    file_list = args.lf
+    return (keyword_file, outdir, file_list)
 
-# print(list_files)
-# list_files = list_files.strip('[]').split(',')
 
-index_dict = {}
-gloss_list = []
-#first get all the gloss keywords
-with open(keyword,'r') as gloss:
-    for key in gloss:
-        key = key.strip()
-        gloss_list.append(key)
-
-#this code is not so efficent now, CAN BE IMPROVED.
-#because Professor changed requirements lately. 
-
-for keyword in gloss_list:
-    for file in list_files:
-        try:
-            with open(file, 'r') as txt:
-                for line in txt:
-
-                    # splits into a list
-                    if keyword in line:
-                        line = line.strip().split(" ")
-                        context = None
-                        index_list = []
-
-                        #iterate over list to handle edge case when 
-                        #keyword ends with punctuation
-                        for index, word in enumerate(line):
-                            if keyword in word:
-                                index_list.append(index)
-        
-                        for index in index_list:
-                            #if keyword appears more than once in a line
-                            key_index = index
-        
-                            if 0 < key_index < len(line) - 1:
-        
-                                context = (line[key_index-1] + " " +
-                                        line[key_index] + " " +
-                                        line[key_index+1])
-        
-                            elif key_index == 0:
-                                if len(line) > 1:
-                                    context = (line[key_index] + " " + 
-                                            line[key_index+1])
-                                else:
-                                    context = line[key_index]
-        
-                            elif key_index == len(line) - 1:
-                                context = (line[key_index - 1] + " " + 
-                                        line[key_index])
-        
-                            if file not in index_dict:
-                                index_dict[file] = []
-
-                            index_dict[file].append(context)
-
-        except IOError as ioe:
-            print("Error opening the file:", ioe)
-            exit(1)
-    
-    output_name = output_file+"." + keyword + ".txt"
+def output_context(outdir, keyword, contexts_per_file):
+    """
+        output context of a keyword
+        Args: outdir, keyword, context
+        Returns: None
+    """
+    output_name = outdir + "/" + keyword + ".txt"
     with open(output_name,'w') as f:
-        for key, value in index_dict.items():
+        for key, value in contexts_per_file.items():
             f.write(keyword + " occurs in: \n")
             for each_context in value:
                 f.write("    " + key + ": " + each_context +"\n")
         f.write("  ")
-        index_dict = {}
+
+
+if __name__ == '__main__':
+    # get command line params:
+    (keyword_file, outdir, file_list) = process_args()
+
+# for debugging:
+# print(list_files)
+# list_files = list_files.strip('[]').split(',')
+
+    contexts_per_file = {}
+    gloss_list = []
+    # first get all the gloss keywords
+    with open(keyword_file,'r') as gloss:
+        for key in gloss:
+            key = key.strip()
+            gloss_list.append(key)
+
+    for keyword in gloss_list:
+        for file in file_list: # look for keywords in all files
+            contexts_per_file[file] = []
+            process_file(file, contexts_per_file)
+
+        output_context(outdir, keyword, contexts_per_file)
+        contexts_per_file = {}  # blank to get ready for next keyword
