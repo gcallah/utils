64e07b3cbe9502f85edf5076d879685cbec4e449
cyong813 <cy986@nyu.edu>
2018-10-27 00:17:51 -0400

    refine and clean url checker

--- url_checker.py
+++ url_checker.py
@@ -3,57 +3,53 @@ Script to check validity of a given file with a lists of URLs.
 Prints request response back.
 """
 
-import sys
-import os
-import string
-import re
 import argparse
 import urllib.request as req
-from urllib.parse import urlparse, urljoin
 
 ARG_ERROR = 1 # type: int
 PARSE_ERROR = 2 # type: int
 IO_ERROR = 3  # type: int
 
 def is_accessible(link): # type: (str) -> bool
+    """
+    Function that accesses a url string and returns response status code.
+    """
     try:
-        if link.startswith('http') or link.startswith('https'):     
+        if link.startswith('http') or link.startswith('https'):
             connection = req.urlopen(link)
-            return connection.getcode()
-        elif link.startswith('/'): 
+            print(connection.getcode())
+        elif link.startswith('/'):
             rel_link = "http://www.thedevopscourse.com" + link
             connection = req.urlopen(rel_link)
-            return connection.getcode()
-        else: 
+            print(connection.getcode())
+        else:
             rel_link_two = "http://www.thedevopscourse.com/" + link
             connection = req.urlopen(rel_link_two)
-            return connection.getcode()
-    except req.HTTPError as e:
-        return e.getcode()
-    except req.URLError as e:
-        return "Invalid URL. No response code because address doesn't exist."
-    
-def process_url_results(url_arr):
-    for url in url_arr:
-        print(is_accessible(url))
+            print(connection.getcode())
+    except req.HTTPError as http_e:
+        print(http_e.getcode())
+    except req.URLError:
+        print("Invalid URL. No response code because address doesn't exist.")
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("url_inp_file", help="text url input file to be parsed")
     args = arg_parser.parse_args()
-    url_inp_file = args.url_inp_file
+    URL_INP_FILE = args.url_inp_file
 
-    url_list = []
+    URL_LIST = []
 
     try:
         # get all the url links
-        with open(url_inp_file,'r') as urls:
-            for link in urls:
-                link = link.strip()
-                url_list.append(link)
+        with open(URL_INP_FILE, 'r') as urls:
+            for url_link in urls:
+                url_link = url_link.strip()
+                URL_LIST.append(url_link)
     except IOError:
-        print("Couldn't read " + url_inp_file)
-        exit(IO_ERROR)    
+        print("Couldn't read " + URL_INP_FILE)
+        exit(IO_ERROR)
 
-    process_url_results(url_list)
+    for url in URL_LIST:
+        is_accessible(url)
+
     exit(0)
