dffb28e615b9925c436538f2adc6cc1169b6c934
gcallah <gcallah@mac.com>
2018-10-29 15:42:54 -0400

    Added space after colon in output.

--- create_gloss.py
+++ create_gloss.py
@@ -46,7 +46,7 @@ gloss_list = []
 for key in d:
     # gloss_item = '<a name=' + key + '>'
     gloss_item = '<span class="hilight" id="'
-    gloss_item += key + '">' + key + '</span>:'
+    gloss_item += key + '">' + key + '</span>: '
     # gloss_item += '</a> '
     gloss_item += d[key]
     gloss_list.append(gloss_item)
