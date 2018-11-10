44c7f48efd3abb89445f5cd3a4dee361c710c42f
Mayank Lamba <lamba.mayank94@gmail.com>
2018-11-10 18:41:40 -0500

    Added a customized test for gloss_links.py.

--- tests/all_tests.sh
+++ tests/all_tests.sh
@@ -10,6 +10,13 @@ diff_output() {
     rm $TEST_DATA/$1_tmp.txt
 }
 
+diff_output_gloss_links() {
+    echo "Going to diff $TEST_DATA/$1_out.txt $TEST_DATA/$2.txt"
+    diff $TEST_DATA/$1_out.txt $TEST_DATA/$2.txt
+    echo "$1 passed."
+    rm $TEST_DATA/$2.txt
+}
+
 run_diff_test_std() {
     echo "Running python ./$1.py $2 < $TEST_DATA/$1_inp.txt > $TEST_DATA/$1_tmp.txt"
     python ./"$1.py" "$2" < $TEST_DATA/$1_inp.txt > $TEST_DATA/$1_tmp.txt
@@ -28,10 +35,20 @@ run_quiz_test_work() {
     diff_output $1
 }
 
+run_gloss_links_file() {
+    echo "Running python ./$1.py with keyword "$3" > $TEST_DATA/$3.txt"
+    python ./"$1.py" $TEST_DATA/"$2.txt" $TEST_DATA --lf "$TEST_DATA/$1_inp.txt"
+    diff_output_gloss_links $1 $3
+
+}
+
 export title="Test page"
 export title2="work"
+export key="gloss_key"
+export key_word="Django"
 run_diff_test_std create_page "$title"
 run_diff_test_file create_gloss
+run_gloss_links_file gloss_links "$key" "$key_word"
 run_diff_test_file create_menu
 run_diff_test_file html_checker
 # run_quiz_test_work qexport "$title2"
