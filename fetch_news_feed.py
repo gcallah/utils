72c3a0b79dc25fa2e2d73313efccc164a4852b93
Mayank Lamba <lamba.mayank94@gmail.com>
2018-11-17 21:25:37 -0500

    Updated fetch_news_feed.py to conform to flake8.

--- fetch_news_feed.py
+++ fetch_news_feed.py
@@ -8,25 +8,27 @@ try:
 except ImportError:
     print("WARNING: Typing module is not found")
 
-user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7' # type: str
+user_agent = ("Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) "
+              "Gecko/2009021910 Firefox/3.0.7")  # type: str
 headers = {'User-Agent': user_agent, }  # type: Dict[str, object]
-request = urllib2.Request("https://devops.com", None, headers) # type: object
-response = urllib2.urlopen(request, None, 5) # type: object
-html = response.read()# type: object
-soup = BeautifulSoup(html) # type: object
-news = {} #type: Dict[Any, Any]
+request = urllib2.Request("https://devops.com", None, headers)  # type: object
+response = urllib2.urlopen(request, None, 5)  # type: object
+html = response.read()  # type: object
+soup = BeautifulSoup(html)  # type: object
+news = {}  # type: Dict[Any, Any]
 
 for tag in soup.find_all(attrs={"class": "slides"}):
     for atag in tag.find_all('a'):
-        print (atag['href'])
-        print (atag.find('img')['src'])
+        print(atag['href'])
+        print(atag.find('img')['src'])
         news[atag['href']] = atag.find('img')['src']
 
 index_page = BeautifulSoup(open("../index.html"), "html.parser")
-count = 0 #type: int
+count = 0  # type: int
 
 for tag in index_page.find_all(attrs={"class": "carItem"}):
-    tag['onclick'] = "window.location.href='" + list(news.keys())[count] + "'" # type :List[str]
+    tag['onclick'] = ("window.location.href='"
+                      + list(news.keys())[count] + "'")  # type :List[str]
     tag['src'] = news[list(news.keys())[count]]
     count += 1
 
