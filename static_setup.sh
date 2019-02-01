d766ea48ab15027c440b159cfeac5c3341d4d8e5
Monjur Hasan <monjur.nishad@gmail.com>
2019-01-31 22:39:17 -0500

    updated setup

--- static_setup.sh
+++ static_setup.sh
@@ -9,3 +9,16 @@ dirname=$(echo $1 | sed 's/.*\/\([A-Za-z0-9]*\)\.git/\1/')
 #dirname=OOP2
 echo $dirname
 echo "Dir name = $dirname"
+
+mkdir html_src
+mkdir docker 
+mkdir templates
+mkdir tests
+
+cp utils/templates/index.ptml html_src/index.ptml
+cp utils/templates/makefile makefile 
+cp utils/templates/head.txt templates/head.txt
+cp utils/templates/menu.txt templates/menu.txt
+cp utils/templates/logo.txt templates/logo.txt
+
+git submodule add https://github.com/gcallah/utils
