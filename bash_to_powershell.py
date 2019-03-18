a4658c9ec62ace327d6342bde997538bbaf7744d
Dhaval Popat <dhavalpopat10@gmail.com>
2019-03-17 21:12:18 -0400

    Bash to Powershell: Added support for "if-elif-else"

--- bash_to_powershell.py
+++ bash_to_powershell.py
@@ -11,12 +11,37 @@ newFileName = str(fileName).split(".sh")[0] + "_converted.sh"
 fileContent = open(fileName, "r")
 convertedFile = open(newFileName, "w+")
 
+def replaceConditions(s):
+    s = s.replace("[", "(", 1)
+    s = s[::-1].replace("]", ")")[::-1]
+    s = s.replace("[", "")
+    s = s[::-1].replace("]", "")
+    s = s.replace("=", "-eq")
+    s = s.replace("==", "-eq")
+    s = s.replace("!=", "-ne")
+    s = s.replace("&&", "-and")
+    s = s.replace("||", "-or")
+    return s
+
 for line in fileContent:
     line = line.strip()
     if line == "set -e":
         line = "$erroractionpreference = \"stop\""
     elif line.startswith("export "):
-        line.replace("export ", "$", 1)
+        line = line.replace("export ", "$", 1)
+    elif line.startswith("if"):
+        line = replaceConditions(line)
+    elif line.startswith("elif"):
+        line = "} \n" + line
+        line = line.replace("elif", "elseif")
+        line = replaceConditions(line)
+    elif line.startswith("then"):
+        line = "{"
+    elif line.startswith("else"):
+        line = "} \n" + line
+        line = line.replace("else", "else {")
+    elif line.startswith("fi"):
+        line = "}"
     convertedFile.write(line + "\n")
 
 fileContent.close()
--- tests/unit_tests_converted.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#  This file should include all tests run to unit test code modules.
-
-# exit on any error with that error status:
-$erroractionpreference = "stop"
-
-exit 0
