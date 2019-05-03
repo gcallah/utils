4cd3d07673de74df30c399a47c5194149c456c6f
Gauri Sarode <32345070+gaurisarode@users.noreply.github.com>
2019-05-03 11:49:57 -0400

    Fixing errors on html tags

--- pylib/tests/test_html_tags.py
+++ pylib/tests/test_html_tags.py
@@ -18,42 +18,40 @@ class HtmlTagTestCase(TestCase):
         str1 = "{% include ' filename ' %}"
         file1 = "filename"
         self.assertEqual(include_tag(str1), file1)
-    
-    def par(self):
-        self.assertEqual(1,1)
+		
+	def par(self):
+        self.assertEqual(1, 1)
 
     def link(self):
-        self.assertEqual(1,1)
+        self.assertEqual(1, 1)
 		
     def figure(self):
-        self.assertEqual(1,1)
+        self.assertEqual(1, 1)
 		
     def details(self):
-        self.assertEqual(1,1)
+        self.assertEqual(1, 1)
 
     def ulist(self):
-        self.assertEqual(1,1)
+        self.assertEqual(1, 1)
 
     def olist(self):
-        self.assertEqual(1,1)
+        self.assertEqual(1, 1)
 
     def image(self):
-        self.assertEqual(1,1)
+        self.assertEqual(1, 1)
 		
     def head(self):
-        self.assertEqual(1,1)
+        self.assertEqual(1, 1)
 		
     def sidebar_links(self):
-        self.assertEqual(1,1)
+        self.assertEqual(1, 1)
 		
     def sidebar(self):
-        self.assertEqual(1,1)
+        self.assertEqual(1, 1)
 		
     def str_to_valid_id(self):
-        self.assertEqual(1,1)
+        self.assertEqual(1, 1)
 		
     def html_list(self):
-        self.assertEqual(1,1)
-
-		
+        self.assertEqual(1, 1)
 		
