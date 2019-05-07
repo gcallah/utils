3f0c0055ec6093d91c83dc630da2d2a4b0161e00
gcallah <gcallah@mac.com>
2019-05-07 00:48:39 -0400

    Ready to try it on real code.

--- cpp2html.py
+++ cpp2html.py
@@ -11,8 +11,8 @@ import re
 import pylib.create_page as pyl
 import pylib.html_tags as html
 
-COMMENT_START = re.compile("^\s/\*\s*$")
-COMMENT_END = re.compile("^\s\* \*/\s*$")
+COMMENT_START = re.compile("^\s*/\*\s*$")
+COMMENT_END = re.compile("^\s*\* \*/\s*$")
 
 
 if len(sys.argv) < 2:
@@ -21,10 +21,32 @@ if len(sys.argv) < 2:
 
 cpp_file = sys.argv[1]
 
-print(pyl.html_start_stuff(cpp_file))
+print(pyl.ptml_start_stuff(cpp_file))
 with open(cpp_file, 'r') as inp:
-    in_comment = False
+    # we will find ourselves in "regular" text (long comments)
+    # or in code.
+    text = ""
+    in_reg_text = False
     for line in inp:
-        print(html.par(line))
+        if COMMENT_START.match(line):
+            # print("STARTING COMMENT")
+            if len(text):
+                print(html.code_par(text))
+            in_reg_text = True
+            text = ""
+            continue
+        elif COMMENT_END.match(line):
+            # print("ENDING COMMENT")
+            in_reg_text = False
+            print(html.par(text))
+            text = ""
+            continue
+        if in_reg_text:
+            text += line.replace("*", "")
+        else:
+            text += line
+
+    if len(text):
+        print(html.code_par(text))
 
 print(pyl.html_end_stuff())
