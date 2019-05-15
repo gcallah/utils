7b799a8138a8c12d9b0b004b37d852a31659720d
gcallah <gcallah@mac.com>
2019-05-15 06:33:39 -0400

    Trying syntax hilighting.

--- cpp2html.py
+++ cpp2html.py
@@ -7,6 +7,9 @@ file and turn it into a web page.
 
 import sys
 import re
+import pygments
+import pygments.formatters
+from pygments.lexers.c_cpp import CppLexer
 
 import pylib.create_page as pyl
 import pylib.html_tags as html
@@ -14,46 +17,64 @@ import pylib.html_tags as html
 COMMENT_START = re.compile("^\s*/\*\s*$")
 COMMENT_END = re.compile("^\s*\* \*/\s*$")
 
+class InlineHtmlFormatter(pygments.formatters.HtmlFormatter):
+    """
+    Use pygments to format html code inline.
+    """
+    def wrap(self, source, _outfile):
+        return self._wrap_code(source)
 
-if len(sys.argv) < 2:
-    print("Must supply a C++ file.")
-    exit(1)
+    @staticmethod
+    def _wrap_code(source):
+        yield from source
 
-cpp_file = sys.argv[1]
 
-print(pyl.ptml_start_stuff(cpp_file))
-with open(cpp_file, 'r') as inp:
-    # we will find ourselves in "regular" text (long comments)
-    # or in code.
-    text = ""
-    in_reg_text = False
-    consec_blanks = 0
-    for line in inp:
-        if COMMENT_START.match(line):
-            if len(text):
-                # no extra line after code!
-                print(html.code_par(text), end="")
-            in_reg_text = True
-            text = ""
-            continue
-        elif COMMENT_END.match(line):
-            in_reg_text = False
-            print(html.par(text))
-            text = ""
-            continue
-        if in_reg_text:
-            line = line.replace("*", "")
-        else:
-            if not line.strip():
-                consec_blanks += 1
-            if consec_blanks >= 2:
-                consec_blanks = 0
+def main():
+    if len(sys.argv) < 2:
+        print("Must supply a C++ file.")
+        exit(1)
+
+    cpp_file = sys.argv[1]
+    cpp_lexer = CppLexer()
+
+    print(pyl.ptml_start_stuff(cpp_file))
+    with open(cpp_file, 'r') as inp:
+        # we will find ourselves in "regular" text (long comments)
+        # or in code.
+        text = ""
+        in_reg_text = False
+        consec_blanks = 0
+        for line in inp:
+            if COMMENT_START.match(line):
+                if len(text):
+                    # no extra line after code!
+                    print(html.code_par(text), end="")
+                in_reg_text = True
+                text = ""
                 continue
-            line = line.replace("<", "&lt;")
-            line = line.replace(">", "&gt;")
-        text += line
+            elif COMMENT_END.match(line):
+                in_reg_text = False
+                print(html.par(text))
+                text = ""
+                continue
+            if in_reg_text:
+                line = line.replace("*", "")
+            else:
+                if not line.strip():
+                    consec_blanks += 1
+                if consec_blanks >= 2:
+                    consec_blanks = 0
+                    continue
+            if not in_reg_text:
+                line = pygments.highlight(line, cpp_lexer,
+                                   InlineHtmlFormatter())
+            text += line
 
-    if len(text):
-        print(html.code_par(text))
+        if len(text):
+            print(html.code_par(text))
 
-print(pyl.html_end_stuff())
+    print(pyl.html_end_stuff())
+
+
+if __name__ == '__main__':
+    main()
