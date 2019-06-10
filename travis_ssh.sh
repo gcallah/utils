8fe1760624c91fde7de5a9f277f3ff67ca1f5029
gcallah <gcallah@mac.com>
2019-06-10 12:24:26 -0400

    Changed  to com_or_org.

--- travis_ssh.sh
+++ travis_ssh.sh
@@ -4,22 +4,22 @@
 
 if [[ -z "$1" || -z "$2" || -z "$3" || -z "$4" ]]
 then
-    echo "Usage: travis-ssh.sh [keyfile] [ssh_user] [deploy_host] [repo]."
+    echo "Usage: travis-ssh.sh [keyfile] [ssh_user] [deploy_host] [org or com]."
     exit 1
 fi
 
 key=~/.ssh/$1
 user=$2
 host=$3
-repo=$4
+org_or_com=$4
 
 echo "Generating ssh key."
 ssh-keygen -t rsa -b 2048 -C 'build@travis-ci.org' -f $key
 
 echo "Registering key with Travis -- you may need to login to Travis first!"
 echo "(For login, use 'travis login --com' [or --org if you are hosted on .org)"
-travis login --com
-travis encrypt-file $key --add --com -r $repo
+travis login --$org_or_com
+travis encrypt-file $key --add --$org_or_com -r $repo
 git add $1.enc
 
 echo "Deploying key to server host."
