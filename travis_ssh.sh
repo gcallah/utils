a034c5ac32e2b175411efd08107522633cb85ea8
gcallah <gcallah@mac.com>
2019-06-10 12:36:16 -0400

    Took superfluous comment out of travis ssh script.

--- travis_ssh.sh
+++ travis_ssh.sh
@@ -17,7 +17,6 @@ echo "Generating ssh key."
 ssh-keygen -t rsa -b 2048 -C 'build@travis-ci.org' -f $key
 
 echo "Registering key with Travis -- you may need to login to Travis first!"
-echo "(For login, use 'travis login --com' [or --org if you are hosted on .org)"
 travis login --$org_or_com
 travis encrypt-file $key --add --$org_or_com -r $repo
 git add $1.enc
