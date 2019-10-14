d378a3961aabbe4c379d549bd1c8547b9a875732
wm1065 <wm1065@nyu.edu>
2019-10-14 00:05:52 -0400

    Finished up the task that guards against overwriting existing work.

--- static_setup.sh
+++ static_setup.sh
@@ -5,8 +5,15 @@
 
 add_file()
 {
+    echo "we are going to add" $2
     cp $1/$2 $3/$2
     cd $3; git add $2; cd -
+}
+
+add_dir()
+{
+    echo "we are going to make directory:" $1
+    mkdir $1
 }
 
 if [[ -z "$1" ]]
@@ -31,16 +38,10 @@ fi
 echo "We are going to add utils as a submodule."
 git submodule add https://github.com/gcallah/utils.git
 
-if [ -d "$newdir/html_src" ]
-then
-    echo "$newdir/html_src exists."
-else
-    echo "we are going to try to make: $newdir/html_src "
-    mkdir $newdir/html_src
-fi
-mkdir $newdir/docker 
-mkdir $newdir/templates
-mkdir $newdir/tests
+[ -d $newdir/html_src ] && echo "$newdir/html_src exists." || add_dir $newdir/html_src
+[ -d $newdir/templates ] && echo "$newdir/templates exists." || add_dir $newdir/templates
+[ -d $newdir/docker ] && echo "$newdir/docker exists." || add_dir $newdir/docker
+[ -d $newdir/tests ] && echo "$newdir/tests exists." || add_dir $newdir/tests
 
 utilsdir=utils
 if [ -n "$2" ]; then
@@ -48,13 +49,15 @@ if [ -n "$2" ]; then
 fi
 echo "utils dir is $utilsdir"
 
-add_file "$utilsdir/templates" style.css $newdir
-add_file "$utilsdir/templates" index.ptml "$newdir/html_src"
-add_file "$utilsdir/templates" about.ptml "$newdir/html_src"
-add_file "$utilsdir/templates" makefile "$newdir"
-add_file "$utilsdir/templates" head.txt "$newdir/templates"
-add_file "$utilsdir/templates" menu.txt "$newdir/templates"
-add_file "$utilsdir/templates" logo.txt "$newdir/templates"
+[ -f $newdir/style.css ] && echo "style.css exists." || add_file "$utilsdir/templates" style.css $newdir
+[ -f $newdir/html_src/index.ptml ] && echo "index.ptml exists." || add_file "$utilsdir/templates" index.ptml "$newdir/html_src"
+[ -f $newdir/html_src/about.ptml ] && echo "about.ptml exists." || add_file "$utilsdir/templates" about.ptml "$newdir/html_src"
+[ -f $newdir/makefile ] && echo "makefile exists." || add_file "$utilsdir/templates" makefile "$newdir"
+[ -f $newdir/templates/head.txt ] && echo "head.txt exists." || add_file "$utilsdir/templates" head.txt "$newdir/templates"
+[ -f $newdir/templates/menu.txt ] && echo "menu.txt exists." || add_file "$utilsdir/templates" menu.txt "$newdir/templates"
+[ -f $newdir/templates/logo.txt ] && echo "logo.txt exists." || add_file "$utilsdir/templates" logo.txt "$newdir/templates"
+[ -f $newdir/docker/Dockerfile ] && echo "Dockerfile exists." || add_file "$utilsdir/docker" Dockerfile "$newdir/docker"
+[ -f $newdir/docker/requirements.txt ] && echo "requirements.txt exists." || add_file "$utilsdir/docker" requirements.txt "$newdir/docker"
 
 # make cloning utils an option!
 cd $newdir; git submodule add https://github.com/gcallah/utils
