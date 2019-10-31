11651e5caa31008ae99f68531a5b34efcaf15ffc
Kevin Hu <kh2547@nyu.edu>
2019-10-31 11:56:49 -0400

    checks for existing directory, else creates one

--- flask_setup/initial_flask_setup.sh
+++ flask_setup/initial_flask_setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-virtualenv -q -p /user/bin/python3.7 $1
-$1/bin/pip install -r requirements.txt
+
+if [[ -n $1 ]]; then
+	mkdir -p "$1"
+else
+	echo "must enter in directory name"
+fi
+
+
+#virtualenv -q -p /user/bin/python3.7 $1
+#$1/bin/pip install -r requirements.txt
