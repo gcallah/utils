42c806540b9fcdcfd70302b359b17ad734061a2e
David Zheng <davidzheng54@gmail.com>
2019-11-02 19:34:58 -0400

    added / route to __init__.py

--- flask_setup/flash_project_layout/flaskr/__init__.py
+++ flask_setup/flash_project_layout/flaskr/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app(test_config=None):
@@ -28,5 +28,9 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    @app.route('/')
+    def home():
+        return render_template('index.html')
 
     return app
