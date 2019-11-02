3c24766a5b7fb0ce43f4c1171729f0d2fec5a251
David Zheng <davidzheng54@gmail.com>
2019-11-02 00:20:53 -0400

    created generic flask folder structure, change initial_flash_setup.sh to install python3 and copy flask folder to project folder

--- flask_setup/initial_flask_setup.sh
+++ flask_setup/initial_flask_setup.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-# should probably say something if directory already exists
+# Variables
+projectDir=
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# ================= Helper Functions
+# copies file from script directory to a destination
+copyFileToDir() 
+{
+	cp $scriptDir/$1 $2
+}
+
+# should probably say something if directory already exists
 if [[ -n $1 ]]; then
 	mkdir -p "$1"
-
+	projectDir=$1
 else
 	echo "must enter in directory name"
+	exit 1
 fi
 
-echo "checking for python and pip"
-
-sudo apt install python
-sudo apt-get install python-pip
+echo "attempting to install python and pip"
+sudo apt-get install python3
+sudo apt-get install python3-pip
 
 #should we use virtual environments
-echo "checking for virtualenv"
+echo "attempting to install virtualenv & dependencies from requirements.txt"
 sudo apt-get install virtualenv
 sudo pip install -r requirements.txt
 
-
+#Copies our generic project folder structure to project directory
+cp $scriptDir/flash_project_layout/* $projectDir
