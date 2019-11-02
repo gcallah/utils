6446ddce587b20a0c0a8a3e4652f138710946bcf
David Zheng <davidzheng54@gmail.com>
2019-11-02 19:21:39 -0400

    made initial_flask_setup.sh have idempotency for writes to bashrc file for environment variables

--- flask_setup/initial_flask_setup.sh
+++ flask_setup/initial_flask_setup.sh
@@ -21,13 +21,24 @@ else
 fi
 
 echo "attempting to install python and pip"
-sudo apt-get install python3
-sudo apt-get install python3-pip
+sudo apt-get install python
+sudo apt-get install python-pip
 
 #should we use virtual environments
 echo "attempting to install virtualenv & dependencies from requirements.txt"
 sudo apt-get install virtualenv
-sudo pip3 install -r requirements.txt
+sudo pip install -r requirements.txt
 
 #Copies our generic project folder structure to project directory
 cp -r $scriptDir/flash_project_layout/* $projectDir
+
+echo "$FLASK_APP"
+#Append flask environment variables to ~/.bashrc
+while IFS="=" read -r key val
+do
+	varExists="$( cat ~/.bashrc | grep "export $key=")"
+	if [[ -z "${varExists}" ]]; then
+		echo "Setting $key"
+		echo "export $key=$val" >> ~/.bashrc
+	fi
+done < "$scriptDir/env.txt"
