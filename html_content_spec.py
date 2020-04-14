85d7e54c7f91ad6bdfd18ae74766aff548dcb0e3
David Zheng <davidzheng54@gmail.com>
2020-04-13 21:21:13 -0400

    added more tags to html_content_spec.py

--- html_content_spec.py
+++ html_content_spec.py
@@ -13,6 +13,8 @@ legal content called the content model
 
 Note: "_EXCEPTIONS" is a custom list of tags to ignore that otherwise might
         not have a clear category in the spec
+Note 2: some tags have special rules that need additional context (attributes, html structure, etc)
+        These rules are not captured within the content_spec.
 """
 
 '''
@@ -98,6 +100,11 @@ content_spec = {"p":
                         "categories" : ["metadata", "flow","phrasing","script-supporting"],
                         "content_model" : [_ANY_CONTENT]
                     },
+                "form":
+                    {
+                        "categories" : ["flow", "palpable"],
+                        "content_model" : ["flow"]
+                    },
                 "img":
                     {
                         "categories" : ["flow","phrasing","embedded","form-associated","palpable","interactive"],
@@ -137,6 +144,98 @@ content_spec = {"p":
                     {
                         "categories" : [_NONE_GROUP],
                         "content_model" : ["flow"]
+                    },
+                "link":
+                    {
+                        # has special categories depending on attribute as well (not covered)
+                        "categories" : ["metadata"],
+                        "content_model" : [_NO_CONTENT]
+                    },
+                "title":
+                    {
+                        "categories" : ["metadata"],
+                        "content_model" : [_NO_CONTENT]
+                    },
+                "button":
+                    {
+                        # This has special rules (not )
+                        "categories" : ["flow","phrasing", "palpable","interactive"],
+                        "content_model" : ["phrasing"]
+                    },
+                "br":
+                    {
+                        "categories" : ["flow","phrasing"],
+                        "content_model" : [_NO_CONTENT]
+                    },
+                "label":
+                    {
+                        "categories" : ["flow","phrasing","interactive","palpable"],
+                        "content_model" : ["phrasing"]
+                    },
+                "textarea":
+                    {
+                        "categories" : ["flow","phrasing","interactive", "palpable"],
+                        "content_model" : [_NO_CONTENT]
+                    },
+                "strong":
+                    {
+                        "categories" : ["flow","phrasing","palpable"],
+                        "content_model" : ["phrasing"]
+                    },
+                "small":
+                    {
+                        "categories" : ["flow","phrasing","palpable"],
+                        "content_model" : ["phrasing"]
+                    },
+                "i":
+                    {
+                        "categories" : ["flow","phrasing","palpable"],
+                        "content_model" : ["phrasing"]
+                    },
+                "span":
+                    {
+                        "categories" : ["flow","phrasing","palpable"],
+                        "content_model" : ["phrasing"]
+                    },
+                "article":
+                    {
+                        "categories" : ["flow","sectioning","palpable"],
+                        "content_model" : ["flow"]
+                    },
+                "section":
+                    {
+                        "categories" : ["flow","sectioning","palpable"],
+                        "content_model" : ["flow"]
+                    },
+                "nav":
+                    {
+                        "categories" : ["flow","sectioning","palpable"],
+                        "content_model" : ["flow"]
+                    },
+                "aside":
+                    {
+                        "categories" : ["flow","sectioning","palpable"],
+                        "content_model" : ["flow"]
+                    },
+                "iframe":
+                    {
+                        "categories" : ["flow","phrasing","embedded","interactive","palpable"],
+                        "content_model" : [_NO_CONTENT]
+                    },
+                "code":
+                    {
+                        "categories" : ["flow", "phrasing", "palpable"],
+                        "content_model" : ["phrasing"]
+                    },
+                "header":
+                    {
+                        "categories" : ["flow", "palpable"],
+                        "content_model" : ["flow"]
+                    },
+                "em":
+                    {
+                        "categories" : ["flow","phrasing", "palpable"],
+                        "content_model" : ["phrasing"]
                     },
                 "_EXCEPTIONS":
                     { "html" , "body", "title", "head"}
