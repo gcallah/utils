e51cf34c53ed7868f756d378f92c7c443ac979da
David Zheng <davidzheng54@gmail.com>
2020-04-17 23:03:00 -0400

    quiz_combiner, shuffles answer choices per question

--- quiz_combiner.py
+++ quiz_combiner.py
@@ -3,38 +3,80 @@
 Script to read in quiz questions from different files, combines and randomizes the questions
 """
 
+"""
+Usage: quiz_combiner.py [file ...]
+Outputs results to stdout
+"""
+
 import sys
 import random
+import string
 
+lowerAlphaList = list(string.ascii_lowercase)
 questionList = []
 
-class Question:
+class Choices:
+    '''
+        represents a list of choices (max of 26 choices)
+    '''
+    def __init__(self, choices):
+        self.list = choices
+    
+    # Shuffles n-1 choices, leaving nth choice unshuffled
+    def shuffleChoices(self):
+        numSample = len(self.list)-1
+        temp = random.sample(self.list[:numSample], k=numSample)
 
-    # Class representing a question
-    # __init__(str,str,str)
-    def __init__(self, question, choices, points):
-        # Note each are strings
+        # Copy the results
+        for i in range(len(temp)):
+            self.list[i] = temp[i]
+
+    # each time you print, the choices get shuffled
+    def __str__(self):
+        # shuffle the choices
+        self.shuffleChoices()
+
+        # Then format the results accordingly
+        result = ""
+        for choice, letter in zip(self.list, lowerAlphaList):
+            # If it is the answer
+            if(choice[1] == True):
+                result += "*"
+            
+            result = result + letter + ". " + choice[0]
+
+        return result
+
+class Question:
+    '''
+        represents a question
+    '''
+
+    # __init__(str,obj(Choices),str) -> None
+    def __init__(self, question, choices, points): 
         self.question = question
-        self.choices = choices
+        self.choices = str(choices)
         self.points = points
 
     def __str__(self):
         # each should have their own newline character
-        return "{self.points}{self.question}{self.choices}".format(self=self)
+        return "{self.points}{self.question}\n{self.choices}".format(self=self)
 
 
 def parseFiles(filenames):
     for file in filenames:
-        print("Processing %s" % file)
         with open(file,'r') as inputStream:
             line = inputStream.readline()
 
             # Processes per question (each separated by a newline)
             while(line):
+                # Skip over trailing newlines between questions
+                while(line == "\n"):
+                    line = inputStream.readline()
+
                 # First, get the points
                 start = line.index(" ")
                 points = line[start+1:]
-                print("POINTS: " + points)
     
                 # Now the question
                 question = ""
@@ -42,23 +84,29 @@ def parseFiles(filenames):
                 while(line != "\n"):
                     question += line
                     line = inputStream.readline()
-                print("QUESTION: " + question)
 
                 # Then the choices
-                choices = ""
+                answerChoices = []
                 line = inputStream.readline()
                 while(line != "\n"):
-                    choices += line
+                    textStart = line.index(" ")
+                    # Mark which choice is the answer and strip away the letter
+                    if(line[0] == '*'):
+                        answerChoices.append((line[textStart+1:], True))
+                    else:
+                        answerChoices.append((line[textStart+1:], False))
+
                     line = inputStream.readline()
-                print("CHOICES: " + choices)
 
                 # Append and make a new instance of a question obj
-                questionList.append(Question(question, choices, points))
+                questionList.append(Question(question, Choices(answerChoices), points))
 
                 # Move to the next section
                 line = inputStream.readline()
 
 if __name__ == "__main__":
+    # Read in the questions from files
+    # Append questions to questionList
     quizFiles = sys.argv[1:]
     parseFiles(quizFiles)
 
