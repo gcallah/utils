e8cca3c58322509ffcd530d0693edae76ed9be15
Denys Fenchenko <denisfench@gmail.com>
2021-02-06 12:49:22 -0500

    Adding the gradescope format option to the quiz generator

--- qexport.py
+++ qexport.py
@@ -41,47 +41,78 @@ def read_questions(mod_nm):
     return recs
 
 
-def write_questions(recs):
+def write_questions(recs, format):
     """
         Args:
             recs: the data to output
         Returns:
             None (for now: we probably want success or error codes)
     """
-    i = 1
-    for question in recs:
-        print(str(i) + ". (1 point)")
-        print(question["text"])
-        print()
+    if format == "nyu":
+        i = 1
+        for question in recs:
+            print(str(i) + ". (1 point)")
+            print(question["text"])
+            print()
 
-        # list of answer options
-        answers = ['answerA', 'answerB', 'answerC', 'answerD', 'answerE']
-        ans_options = [question[i] for i in answers]
+            # list of answer options
+            answers = ['answerA', 'answerB', 'answerC', 'answerD', 'answerE']
+            ans_options = [question[i] for i in answers]
 
-        # separate list for answer option bullets
-        options = ["a.", "b.", "c.", "d.", "e."]
+            # separate list for answer option bullets
+            options = ["a.", "b.", "c.", "d.", "e."]
 
-        # marking the correct answer by '*'
-        correct = question["correct"].lower() + "."
-        options[options.index(correct)] = "*" + options[options.index(correct)]
-        for option in ans_options:
-            if option:
-                # matching the index for 'options' &
-                # 'ans_options' to get correct alphabet
-                print(options[ans_options.index(option)] + " " + option)
-            else:
-                break
-        i += 1
-        print()
+            # marking the correct answer by '*'
+            correct = question["correct"].lower() + "."
+            options[options.index(correct)] = "*" + options[options.index(correct)]
+            for option in ans_options:
+                if option:
+                    # matching the index for 'options' &
+                    # 'ans_options' to get correct alphabet
+                    print(options[ans_options.index(option)] + " " + option)
+                else:
+                    break
+            i += 1
+            print()
+    if format == "gradescope":
+        i = 1
+        for question in recs:
+            print(question["text"])
+            print()
+
+            # list of answer options
+            answers = ['answerA', 'answerB', 'answerC', 'answerD', 'answerE']
+            ans_options = [question[i] for i in answers]
+
+            # separate list for answer option bullets
+            options = ["a", "b", "c", "d", "e"]
+
+            # marking the correct answer by '*'
+            correct = question["correct"].lower()
+            options[options.index(correct)] = "(X)"
+            for option in range(len(options)):
+                if options[option] != "(X)":
+                    options[option] = "( )"
+            for option in ans_options:
+                if option:
+                    # matching the index for 'options' &
+                    # 'ans_options' to get correct alphabet
+                    print(options[ans_options.index(option)] + " " + option)
+                else:
+                    break
+            i += 1
+            print()
 
 
 def main():
     mod_nm = None
+    format = None
     if len(sys.argv) > 1:
         mod_nm = sys.argv[1]
+        format = sys.argv[2]
 
     recs = read_questions(mod_nm)
-    write_questions(recs)
+    write_questions(recs, format)
 
 
 if __name__ == '__main__':
