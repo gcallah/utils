e9ca92d3062fd563e1b1a16b40766e9fb41c85c1
gcallah <gcallah@mac.com>
2021-04-16 22:26:02 -0400

    Trying to read reviewers from a file.

--- gen_pr_req.py
+++ gen_pr_req.py
@@ -7,7 +7,10 @@ from github import Github, Repository
 import os
 
 TOKEN = 'GITHUB_TOKEN'
-INDRA_REPO = 'INDRA_REPO'
+REPO = 'PR_REPO'
+
+INDRA_REPO = 'IndraABM'
+
 token = os.getenv(TOKEN, None)
 
 BASE_BRANCH = 'master'
@@ -16,14 +19,28 @@ HEAD_BRANCH = 'staging'
 
 ORGANIZATION = 'TandonDevOps'
 
-REPO_NAME = os.getenv(INDRA_REPO, 'IndraABM')
+repo_name = os.getenv(REPO, INDRA_REPO)
 
 """
 Since I (Abhinav) am using my own github account, the PR's are authored by me.
 Due to this, I cannot add myself as a reviewer automatically.
 Solution: Create a GitHub app/bot to create the PR.
 """
-REVIEWERS = ['Denisfench', 'gcallah', 'NathanConroy']
+REV_FILE = 'rev_file'
+
+rev_file = os.getenv(REV_FILE, 'db/reviewers.txt')
+
+DEF_REVIEWERS = ['Denisfench', 'gcallah', 'NathanConroy']
+
+
+def get_reviewers():
+    reviewers = []
+    try:
+        with open(rev_file, 'r') as f:
+            for line in f:
+                reviewers.append(line.strip())
+    except FileNotFoundError:
+        return DEF_REVIEWERS
 
 
 def pr_already_exists(repo: Repository.Repository):
@@ -43,7 +60,7 @@ def generate_pr():
 
     hub = Github(token)
     org = hub.get_organization(login=ORGANIZATION)
-    repo = org.get_repo(name=REPO_NAME)
+    repo = org.get_repo(name=repo_name)
     # Do not try to create a new PR if there is a PR out already.
     pr_exists, pull_requests = pr_already_exists(repo)
     if not pr_exists:
@@ -56,7 +73,8 @@ def generate_pr():
                               body=body,
                               maintainer_can_modify=True, title=title,)
 
-        pr.create_review_request(reviewers=REVIEWERS, team_reviewers=[])
+        pr.create_review_request(reviewers=get_reviewers(),
+                                 team_reviewers=[])
         print(f'Created PR: {pr.html_url}')
     else:
         print('PR already exists. The push should have updated the PR.')
