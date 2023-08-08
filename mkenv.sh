437da100c74552bb3116865bedab24a897407772
gcallah <gcallah@mac.com>
2023-08-08 11:37:00 -0400

    Eliminated spurious new lines.

--- mkenv.sh
+++ mkenv.sh
@@ -6,9 +6,7 @@ python3 -m venv .venv
 
 echo "export PYTHONPATH=$PROJ_DIR" >> .venv/bin/activate
 
-echo "#/bin/sh
-
-source .venv/bin/activate" > act.sh
+echo "#/bin/sh source .venv/bin/activate" > act.sh
 
 chmod 755 act.sh
 
