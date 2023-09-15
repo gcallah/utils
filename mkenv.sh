4e71a6bef29728c9a0b7c420f301492624c86230
gcallah <gcallah@mac.com>
2023-09-15 17:33:11 -0400

    Changed BASE_NAME to PROJ_NAME for clarity.

--- mkenv.sh
+++ mkenv.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 export PROJ_DIR=$(pwd)
-export BASE_NAME=${PWD##*/}
+export PROJ_NAME=${PWD##*/}
 
-export VENV_NAME="$BASE_NAME-venv"
+export VENV_NAME="$PROJ_NAME-venv"
 echo "Creating $VENV_NAME"
 
 python3 -m venv $VENV_NAME
