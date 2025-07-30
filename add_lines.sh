601fbb5caef28654cb0ec141b49c34ef2391282c
gcallah <gcallah@mac.com>
2025-07-30 09:38:15 -0400

    Changed add_lines.sh

--- add_lines.sh
+++ add_lines.sh
@@ -3,6 +3,6 @@
 # add some lines to every file meeting a spec.
 for file in *.md; do
   if [ -f "$file" ]; then
-      awk 'BEGIN {print "\n========"} {print}' "$file" > "$file.tmp" && mv "$file.tmp" "$file"
+      awk 'BEGIN {print "\n========\n\n"} {print}' "$file" > "$file.tmp" && mv "$file.tmp" "$file"
   fi
 done
